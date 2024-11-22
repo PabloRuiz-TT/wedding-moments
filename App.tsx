589ca8d494b6e67593cfceb08323cf8c7c262623
Pablo Ruiz <pablo.ruiz@trafictours.com>
2024-11-21 23:44:10 -0500

    feat, integración de SQLite y creación de tablas en la base de datos

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { KeyboardAvoidingView, Platform, StyleSheet } from "react-native";
-import { PaperProvider } from "react-native-paper";
+import { PaperProvider, ProgressBar } from "react-native-paper";
 import { RootNavigator } from "./src/navigation/RootNavigator";
+import { Suspense } from "react";
+import { SQLiteProvider } from "expo-sqlite";
+import { DATABASE_NAME, migrateDbIfNeeded } from "./src/database/database";
 
 export default function App() {
   return (
@@ -9,11 +12,19 @@ export default function App() {
       style={styles.keyboardAvoidingView}
       behavior={Platform.OS === "ios" ? "padding" : "height"}
     >
-      <PaperProvider>
-        <NavigationContainer>
-          <RootNavigator />
-        </NavigationContainer>
-      </PaperProvider>
+      <Suspense fallback={<ProgressBar indeterminate />}>
+        <SQLiteProvider
+          useSuspense={true}
+          databaseName={DATABASE_NAME}
+          onInit={migrateDbIfNeeded}
+        >
+          <PaperProvider>
+            <NavigationContainer>
+              <RootNavigator />
+            </NavigationContainer>
+          </PaperProvider>
+        </SQLiteProvider>
+      </Suspense>
     </KeyboardAvoidingView>
   );
 }
---
+++ src/database/database.ts
@@ -0,0 +1,104 @@
+import * as SQLite from "expo-sqlite";
+
+export const DATABASE_NAME = "weddings.db";
+
+export async function migrateDbIfNeeded(db: SQLite.SQLiteDatabase) {
+  const DATABASE_VERSION = 1;
+
+  let { user_version: currentDbVersion } = await db.getFirstAsync<any>(
+    "PRAGMA user_version"
+  );
+
+  if (currentDbVersion >= DATABASE_VERSION) {
+    return;
+  }
+
+  if (currentDbVersion === 0) {
+    await db.execAsync(
+      `
+      PRAGMA journal_mode = 'wal';
+      
+      CREATE TABLE IF NOT EXISTS usuarios (
+          UsuarioID INTEGER PRIMARY KEY AUTOINCREMENT,
+          Nombre TEXT NOT NULL,
+          Correo TEXT UNIQUE NOT NULL,
+          Password TEXT NOT NULL,
+          Rol TEXT NOT NULL,
+          FechaRegistro TEXT DEFAULT CURRENT_TIMESTAMP
+        );
+        
+        CREATE TABLE IF NOT EXISTS eventos (
+          EventoID INTEGER PRIMARY KEY AUTOINCREMENT,
+          Titulo TEXT NOT NULL,
+          Descripcion TEXT,
+          FechaEvento TEXT NOT NULL,
+          Ubicacion TEXT,
+          AdministradorID INTEGER,
+          FechaCreacion TEXT DEFAULT CURRENT_TIMESTAMP,
+          FOREIGN KEY (AdministradorID) REFERENCES Usuarios(UsuarioID)
+        );
+        
+        CREATE TABLE IF NOT EXISTS itinerarios (
+          ItinerarioID INTEGER PRIMARY KEY AUTOINCREMENT,
+          EventoID INTEGER,
+          Titulo TEXT NOT NULL,
+          Descripcion TEXT,
+          HoraInicio TEXT NOT NULL,
+          HoraFin TEXT NOT NULL,
+          FOREIGN KEY (EventoID) REFERENCES Eventos(EventoID) ON DELETE CASCADE
+        );
+
+        CREATE TABLE IF NOT EXISTS invitados (
+          InvitadoID INTEGER PRIMARY KEY AUTOINCREMENT,
+          EventoID INTEGER,
+          Nombre TEXT NOT NULL,
+          Correo TEXT NOT NULL,
+          ConfirmacionAsistencia TEXT,
+          Comentarios TEXT,
+          FOREIGN KEY (EventoID) REFERENCES Eventos(EventoID) ON DELETE CASCADE
+        );
+
+
+        CREATE TABLE IF NOT EXISTS historialEventos (
+          HistorialID INTEGER PRIMARY KEY AUTOINCREMENT,
+          EventoID INTEGER,
+          Descripcion TEXT NOT NULL,
+          Fecha TEXT DEFAULT CURRENT_TIMESTAMP,
+          FOREIGN KEY (EventoID) REFERENCES Eventos(EventoID) ON DELETE CASCADE
+        );
+
+        CREATE TABLE IF NOT EXISTS comentarios (
+          ComentarioID INTEGER PRIMARY KEY AUTOINCREMENT,
+          EventoID INTEGER,
+          UsuarioID INTEGER,
+          Comentario TEXT NOT NULL,
+          FechaComentario TEXT DEFAULT CURRENT_TIMESTAMP,
+          FOREIGN KEY (EventoID) REFERENCES Eventos(EventoID) ON DELETE CASCADE,
+          FOREIGN KEY (UsuarioID) REFERENCES Usuarios(UsuarioID)
+        );
+
+        CREATE TABLE IF NOT EXISTS recuerdos (
+          RecuerdoID INTEGER PRIMARY KEY AUTOINCREMENT,
+          EventoID INTEGER,
+          UsuarioID INTEGER,
+          Imagen TEXT NOT NULL,
+          Comentario TEXT,
+          FechaSubida TEXT DEFAULT CURRENT_TIMESTAMP,
+          FOREIGN KEY (EventoID) REFERENCES Eventos(EventoID) ON DELETE CASCADE,
+          FOREIGN KEY (UsuarioID) REFERENCES Usuarios(UsuarioID)
+        );
+
+        CREATE TABLE IF NOT EXISTS regalos (
+          RegaloID INTEGER PRIMARY KEY AUTOINCREMENT,
+          EventoID INTEGER,
+          NombreRegalo TEXT NOT NULL,
+          Descripcion TEXT,
+          Precio REAL,
+          EnlaceCompra TEXT,
+          Estado TEXT DEFAULT 'disponible',
+          FOREIGN KEY (EventoID) REFERENCES Eventos(EventoID) ON DELETE CASCADE
+        );
+        `
+    );
+  }
+}
